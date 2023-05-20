1f7707dfe90a33b23b7ef0aa4098214b65376cf8
AHTOH2001 <Antonlapytko@gmail.com>
2023-05-20 09:52:02 +0300

    Restored requirements and russo work

--- russo.py
+++ russo.py
@@ -2,13 +2,14 @@ import os
 from time import sleep
 
 import pytesseract
+from thefuzz import fuzz, process
 from PIL import Image
 
 os.environ.setdefault('TESSDATA_PREFIX', 'tessdata')
 
 cures_data = ["ярина", "юритмик", "пантал", "мотилак"]
 cures_data = [el.lower() for el in cures_data]
-CUSTOM_CONFIG = r"-l rus+eng"
+CUSTOM_CONFIG = r" --psm 6 --oem 3 -l rus+eng"
 
 
 def process_file(file_path):
@@ -25,10 +26,47 @@ def process_file(file_path):
         pass
 
 
-while True:
-    for dirpath, dirnames, filenames in os.walk('api/media'):
-        for file_name in filenames:
-            file_path = f'{dirpath}/{file_name}'
-            process_file(file_path)
-            os.rename(file_path, f'api/media_archive/{file_name}')
-    sleep(1)
+# while True:
+#     for dirpath, dirnames, filenames in os.walk('api/media'):
+#         for file_name in filenames:
+#             file_path = f'{dirpath}/{file_name}'
+#             process_file(file_path)
+#             os.rename(file_path, f'api/media_archive/{file_name}')
+#     sleep(1)
+
+text = '''}
+ш
+у у
+й,
+и :
+КЕ оаЛ "
+——спцЕнит” ' й
+аао оожоя ы к ж кцу о т ВА бр о НО
+@ - сооЗвщкОвСКИЙ завод медищинск “4@
+’ К пча сурая аКОар ВаОВРОССНя
+} ОН Ю Р ) С е
+оо таоле н_&ко К @ ;‹ р кр ех
+аее 7 пленочи — оболос ИЫг ^
+еаьО. оОО а, Гр е ИСКРМГ й
+: оана ао олН аОСВ =
+ке ЗЫТОЕ ЗКа ЛЫЕОе Обиество
+` ° р ыс '-‘ ‘Э ЕИхреп#ратов* м
+ья ‹ @ уа Ис , ИХ
+В .Ъ‚‚ р ГЪ .] ; # }
+Й а _\‚›!,д@ я т ‘_0‚:' .
+л ао ‚'\А—'\_\д*‹ А ь,‚:ъ‘._ъ}__;_ « лЧС у
+ЧОУОБТОК НОКоке ТЫх пленочно' КОЙ о 200 »
+В!
+В :
+Ва
+о
+,
+ЫЕ " &
+ы Ц 'а.› оы'''
+
+med = 'сооЗвщ ОвСКИЙ'
+print(fuzz.ratio(med, text))  # NO
+print(fuzz.partial_ratio(med, text))
+print(fuzz.token_set_ratio(med, text))  # NO
+print(fuzz.partial_token_set_ratio(med, text)) # likely no
+
