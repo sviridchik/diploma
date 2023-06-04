51c75a3e91526a60de087c2c2d929ab7320b94cd
AHTOH2001 <Antonlapytko@gmail.com>
2023-06-04 13:19:56 +0300

    Changed db to mysql

--- api/api/settings.py
+++ api/api/settings.py
@@ -91,14 +91,23 @@ WSGI_APPLICATION = 'api.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'postgres',
+#         'USER': 'postgres',
+#         'PASSWORD': 'postgres',
+#         'HOST': '127.0.0.1',
+#         'PORT': 5432,
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'postgres',
-        'USER': 'postgres',
-        'PASSWORD': 'postgres',
-        'HOST': '127.0.0.1',
-        'PORT': 5432,
+        "ENGINE": "django.db.backends.mysql",
+        'HOST': 'tipul.mysql.pythonanywhere-services.com',
+        "OPTIONS": {
+            "read_default_file": str(BASE_DIR / 'mysql.cnf'),
+        },
     }
 }
 # DATABASES = {
